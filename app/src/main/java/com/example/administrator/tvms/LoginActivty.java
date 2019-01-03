e8bf4ff817b57b3365bd002c8ed68a717ba57e19
EJ ANTON POTOT <zuming1404@gmail.com>
2019-01-03 23:35:52 +0800

    updated drwawer

--- app/src/main/java/com/example/administrator/tvms/LoginActivty.java
+++ app/src/main/java/com/example/administrator/tvms/LoginActivty.java
@@ -1,13 +1,37 @@
 package com.example.administrator.tvms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivty extends AppCompatActivity {
+public class LoginActivty extends AppCompatActivity implements View.OnClickListener {
+
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activty);
+
+        username= (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(this);
+
+    }
+
+    private
+
+    @Override
+    public void onClick(View v) {
+        if(v == login) {
+//            startActivity(new Intent(this, navigationDrawer.class));
+        }
     }
 }
