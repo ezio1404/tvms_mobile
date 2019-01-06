9afa4ece98cb99da8d61eaa70283cb75b9a7aace
EJ ANTON SABAL POTOT <zuming1404@gmail.com>
2019-01-07 01:09:18 +0800

    added homepage Driver And enforcer

---
+++ app/src/main/java/com/example/administrator/tvms/driverDhp.java
@@ -0,0 +1,13 @@
+package com.example.administrator.tvms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class driverDhp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_dhp);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/tvms/enforcerDhp.java
@@ -0,0 +1,13 @@
+package com.example.administrator.tvms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class enforcerDhp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enforcer_dhp);
+    }
+}
