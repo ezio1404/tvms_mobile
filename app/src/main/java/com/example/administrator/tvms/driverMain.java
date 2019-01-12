7d364b29fddeb10be27d262f9d65a0d75c0b4392
EJ ANTON SABAL POTOT <zuming1404@gmail.com>
2019-01-13 02:16:21 +0800

    added enforcer nav drawer,both no FAQS

--- app/src/main/java/com/example/administrator/tvms/driverMain.java
+++ app/src/main/java/com/example/administrator/tvms/driverMain.java
@@ -9,15 +9,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class driverMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.driver_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
---
+++ app/src/main/java/com/example/administrator/tvms/enforcerMain.java
@@ -0,0 +1,81 @@
+package com.example.administrator.tvms;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class enforcerMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enforcer_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new driver_hompage_frag()).commit();
+            navigationView.setCheckedItem(R.id.nav_violator);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_violator:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new enforcer_violator_frag()).commit();
+                break;
+            case R.id.nav_vp:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new enforcer_vp_frag()).commit();
+                break;
+            case R.id.nav_inquire:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new enforcer_inquire_frag()).commit();
+                break;
+            case R.id.nav_eh:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new enforcer_hao_frag()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new enforcer_settings_frag()).commit();
+                break;
+//            case R.id.nav_faqs:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new driverFaq()).commit();
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
