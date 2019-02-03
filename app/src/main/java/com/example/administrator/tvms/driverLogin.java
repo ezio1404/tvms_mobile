ca7598844843cdf67f3543c9e97940368b3ab001
EJ ANTON SABAL POTOT <zuming1404@gmail.com>
2019-02-03 14:50:52 -0500

    bushit

--- app/src/main/java/com/example/administrator/tvms/driverLogin.java
+++ app/src/main/java/com/example/administrator/tvms/driverLogin.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -17,6 +18,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -49,37 +54,59 @@ public class driverLogin extends AppCompatActivity implements View.OnClickListen
     public void onClick(View v) {
         if(v.getId() == R.id.login) {
            String username,password;
-           String URL = "http://localhost:8082/tvms/loginDriver.php";
-            username=this.username.getText().toString();
-            password=this.password.getText().toString();
-            RequestQueue RQ= Volley.newRequestQueue(driverLogin.this);
-            StringRequest SR = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.d("Response", response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    String response="error";
-                    Log.d("Error.Response", response);
-                }
-            }){
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String>  params = new HashMap<String, String>();
-                    params.put("driver_email", username);
-                    params.put("driver_password", password);
-                    return super.getParams();
-                }
-            };
+            username=this.username.getText().toString().trim();
+            password=this.password.getText().toString().trim();
+            login(username,password);
 
-
-            startActivity(new Intent(this, driverMain.class));
+//            startActivity(new Intent(this, driverMain.class));
         }
         if(v.getId() == R.id.btn_log_enf){
             startActivity(new Intent(this,enforcerLogin.class));
         }
+    }
+
+    private void login(String username, String password) {
+        String URL_LOGIN = "http://localhost:8082/tvms/loginDriver.php";
+        StringRequest stringRequest =new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success=jsonObject.getString("Success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("Driver login");
+                            if(success.equals("1")){
+                                for (int i = 0;i <jsonArray.length();i++){
+                                    String username = jsonObject.getString("driver_email");
+                                    String password = jsonObject.getString("driver_password");
+                                    Toast.makeText(driverLogin.this,"Success Login. \nEmail:"+username,Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(driverLogin.this,"Error "+e.toString(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(driverLogin.this,"Error "+error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String>  params= new HashMap<>();
+                params.put("driver_email",username);
+                params.put("driver_password",password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
 
