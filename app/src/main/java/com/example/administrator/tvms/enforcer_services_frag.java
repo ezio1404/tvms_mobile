66c002abc3ca7bad89608236f46be39df84bf834
EJ ANTON POTOT <zuming1404@gmail.com>
2019-02-10 21:04:28 +0800

    workking on enforcer shits

--- app/src/main/java/com/example/administrator/tvms/enforcer_services_frag.java
+++ app/src/main/java/com/example/administrator/tvms/enforcer_services_frag.java
@@ -14,13 +14,14 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class enforcer_services_frag  extends AppCompatActivity implements View.OnClickListener{
-    Button btn_nnp,btn_rnp,btn_ns,btn_nrp,btn_rrp,btn_LTOservices;
-    Button btn_verify,btn_clearance,btn_CTS;
-    Button btn_addPlateNo,btn_requestClearance,btn_requestCTS;
-    EditText mPlateNo,clearance_plateNo;
-    EditText cts_plateNo,cts_address,cts_desc;
+public class enforcer_services_frag  extends AppCompatActivity implements View.OnClickListener {
+    Button btn_nnp, btn_rnp, btn_ns, btn_nrp, btn_rrp, btn_LTOservices;
+    Button btn_verify, btn_clearance, btn_CTS;
+    Button btn_addPlateNo, btn_requestClearance, btn_requestCTS;
+    EditText mPlateNo, clearance_plateNo;
+    EditText cts_plateNo, cts_address, cts_desc;
 
 
     @Override
@@ -30,79 +31,82 @@ public class enforcer_services_frag  extends AppCompatActivity implements View.O
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.lto_services);
 
-        btn_LTOservices= findViewById(R.id.btn_LTOservices);
-        btn_verify= findViewById(R.id.btn_verify);
-        btn_clearance= findViewById(R.id.btn_clearance);
-        btn_CTS= findViewById(R.id.btn_CTS);
-
-        btn_nnp= findViewById(R.id.btn_nnp);
-        btn_rnp= findViewById(R.id.btn_rnp);
-        btn_ns= findViewById(R.id.btn_ns);
-        btn_nrp= findViewById(R.id.btn_nrp);
-        btn_rrp= findViewById(R.id.btn_rrp);
-
+        btn_LTOservices = findViewById(R.id.btn_LTOservices);
+        btn_verify = findViewById(R.id.btn_verify);
+        btn_clearance = findViewById(R.id.btn_clearance);
+        btn_CTS = findViewById(R.id.btn_CTS);
 
         btn_LTOservices.setOnClickListener(this);
         btn_verify.setOnClickListener(this);
         btn_clearance.setOnClickListener(this);
         btn_CTS.setOnClickListener(this);
 
-        btn_nnp.setOnClickListener(this);
-        btn_rnp.setOnClickListener(this);
-        btn_ns.setOnClickListener(this);
-        btn_nrp.setOnClickListener(this);
-        btn_rrp.setOnClickListener(this);
-
 
     }
+
+
+    @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.btn_LTOservices){
-            AlertDialog.Builder mBuilder = new AlertDialog.Builder(enforcer_services_frag.this);
-            mBuilder.setTitle("LTO Services");
-            View mView=getLayoutInflater().inflate(R.layout.lto_services,null);
-            Uri uri = Uri.parse("smsto:2600");
-            Intent smsIntent = new Intent(Intent.ACTION_SENDTO,uri);
-            String sms = null;
-            switch(v.getId()){
-                case R.id.btn_nnp:
-                    sms="LTO NEW NON PRO";
-//                    smsIntent.putExtra("sms_body","LTO NEW NON PRO");
-                    break;
-                case R.id.btn_rnp:
-                    sms="LTO RENEW NON PRO";
-//                    smsIntent.putExtra("sms_body","LTO RENEW NON PRO");
-                    break;
-                case R.id.btn_ns:
-                    sms="LTO NEW STUDENT";
-//                    smsIntent.putExtra("sms_body","LTO NEW STUDENT");
-                    break;
-                case R.id.btn_nrp:
-                    sms="LTO NEW REG PRIVATE";
-//                    smsIntent.putExtra("sms_body","LTO NEW REG PRIVATE");
-                    break;
-                case R.id.btn_rrp:
-                    sms="LTO RENEW REG PRIVATE";
-//                    smsIntent.putExtra("sms_body","LTO RENEW REG PRIVATE");
-                    break;
-            }
+        if (v.getId() == R.id.btn_LTOservices) {
+
+//            Toast.makeText(this,"shit",Toast.LENGTH_LONG).show();
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+            mBuilder.setTitle(getString(R.string.stringLTO));
+            View mView = getLayoutInflater().inflate(R.layout.lto_services, null);
+            btn_nnp = mView.findViewById(R.id.btn_nnp);
+            btn_rnp = mView.findViewById(R.id.btn_rnp);
+            btn_ns = mView.findViewById(R.id.btn_ns);
+            btn_nrp = mView.findViewById(R.id.btn_nrp);
+            btn_rrp = mView.findViewById(R.id.btn_rrp);
+
+            btn_nnp.setOnClickListener(v1 -> {
+                Uri uri = Uri.parse("smsto:2600");
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                smsIntent.putExtra("sms_body", "LTO NEW NON PRO");
+                startActivity(smsIntent);
+            });
+            btn_rnp.setOnClickListener(v1 -> {
+                Uri uri = Uri.parse("smsto:2600");
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                smsIntent.putExtra("sms_body", "LTO RENEW NON PRO");
+                startActivity(smsIntent);
+            });
+            btn_ns.setOnClickListener(v1 -> {
+                Uri uri = Uri.parse("smsto:2600");
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                smsIntent.putExtra("sms_body", "LTO NEW STUDENT");
+                startActivity(smsIntent);
+            });
+            btn_nrp.setOnClickListener(v1 -> {
+                Uri uri = Uri.parse("smsto:2600");
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                smsIntent.putExtra("sms_body", "LTO NEW REG PRIVATE");
+                startActivity(smsIntent);
+            });
+            btn_rrp.setOnClickListener(v1 -> {
+                Uri uri = Uri.parse("smsto:2600");
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                smsIntent.putExtra("sms_body", "LTO RENEW REG PRIVATE");
+                startActivity(smsIntent);
+            });
             mBuilder.setView(mView);
             AlertDialog dialog = mBuilder.create();
             dialog.show();
-            smsIntent.putExtra("sms_body",sms);
-            startActivity(smsIntent);
-        }
-        if(v.getId()==R.id.btn_verify){
-            AlertDialog.Builder mBuilder = new AlertDialog.Builder(enforcer_services_frag.this);
-            mBuilder.setTitle("Verify PlateNo");
-            View mView=getLayoutInflater().inflate(R.layout.enforcer_verify,null);
-            mPlateNo=mView.findViewById(R.id.mPlateNo);
-            btn_addPlateNo=mView.findViewById(R.id.btn_addPlateNo);
 
-            String plateNo=mPlateNo.getText().toString();
+        }
+        if (v.getId() == R.id.btn_verify) {
+            Toast.makeText(this, "shit", Toast.LENGTH_LONG).show();
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+            mBuilder.setTitle("Verify PlateNo");
+            View mView = getLayoutInflater().inflate(R.layout.enforcer_verify, null);
+            mPlateNo = mView.findViewById(R.id.mPlateNo);
+            btn_addPlateNo = mView.findViewById(R.id.btn_addPlateNo);
+
+            String plateNo = mPlateNo.getText().toString();
             btn_addPlateNo.setOnClickListener(v1 -> {
                 Uri uri = Uri.parse("smsto:2600");
-                Intent smsIntent = new Intent(Intent.ACTION_SENDTO,uri);
-                smsIntent.putExtra("sms_body","LTO VEHICLE "+plateNo);
+                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, uri);
+                smsIntent.putExtra("sms_body", "LTO VEHICLE " + plateNo);
                 startActivity(smsIntent);
             });
             mBuilder.setView(mView);
@@ -111,32 +115,34 @@ public class enforcer_services_frag  extends AppCompatActivity implements View.O
 
 
         }
-        if(v.getId()==R.id.btn_clearance){
-            AlertDialog.Builder mBuilder = new AlertDialog.Builder(enforcer_services_frag.this);
+        if (v.getId() == R.id.btn_clearance) {
+//            Toast.makeText(this,"shit",Toast.LENGTH_LONG).show();
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
             mBuilder.setTitle("Vehicle Clearance");
-            View mView=getLayoutInflater().inflate(R.layout.enforcer_clearance,null);
-            clearance_plateNo=mView.findViewById(R.id.clearance_plateNo);
+            View mView = getLayoutInflater().inflate(R.layout.enforcer_clearance, null);
+            clearance_plateNo = mView.findViewById(R.id.clearance_plateNo);
             btn_requestClearance = mView.findViewById(R.id.btn_requestClearance);
 
-            String mPlateNo=clearance_plateNo.getText().toString();
+            String mPlateNo = clearance_plateNo.getText().toString();
             btn_requestClearance.setOnClickListener(v1 -> {
                 //code here to send request to the agency
                 //missing list of agency
             });
 
         }
-        if(v.getId()==R.id.btn_CTS){
-            AlertDialog.Builder mBuilder = new AlertDialog.Builder(enforcer_services_frag.this);
+        if (v.getId() == R.id.btn_CTS) {
+            Toast.makeText(this, "shit", Toast.LENGTH_LONG).show();
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
             mBuilder.setTitle("Clamping/Towing Service");
-            View mView=getLayoutInflater().inflate(R.layout.enforcer_cts,null);
-            cts_plateNo=mView.findViewById(R.id.cts_plateNo);
+            View mView = getLayoutInflater().inflate(R.layout.enforcer_cts, null);
+            cts_plateNo = mView.findViewById(R.id.cts_plateNo);
             cts_address = mView.findViewById(R.id.cts_address);
-            cts_desc=mView.findViewById(R.id.cts_desc);
-            btn_requestCTS=mView.findViewById(R.id.btn_requestCTS);
+            cts_desc = mView.findViewById(R.id.cts_desc);
+            btn_requestCTS = mView.findViewById(R.id.btn_requestCTS);
 //            String mPlateNo,address,desc;
-            String mPlateNo=cts_plateNo.getText().toString();
-            String address=cts_address.getText().toString();
-            String desc=cts_desc.getText().toString();
+            String mPlateNo = cts_plateNo.getText().toString();
+            String address = cts_address.getText().toString();
+            String desc = cts_desc.getText().toString();
             btn_requestCTS.setOnClickListener(v1 -> {
                 //code here to send request to the agency
                 //missing list of agency
